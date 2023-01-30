17cedd71617fea3433eb798daaf28c16a0a3a5f8
Prashanth <prashanth@IBOs-MacBook-Pro-3.local>
2023-01-30 21:10:08 +0530

    Typescript basic variable types-1

---
+++ dist/index.js
@@ -0,0 +1,13 @@
+"use strict";
+let id = 5;
+id = 10;
+let myName = 'Prashant';
+myName = "Yash";
+let valuee = true;
+valuee = false;
+let x = "bro";
+x = true;
+let values = [1, 2, 3];
+values.push(4);
+let person = [1, "bro", true];
+person.push("India");
---
+++ src/index.ts
@@ -0,0 +1,17 @@
+let id: number = 5;
+id = 10;
+
+let myName: string = 'Prashant';
+myName = "Yash";
+
+let valuee: boolean = true;
+valuee = false;
+
+let x: any = "bro";
+x = true;
+
+let values: number[] = [1, 2, 3];
+values.push(4);
+
+let person:any[]=[1,"bro",true];
+person.push("India");
