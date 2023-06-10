7af65218dea1622127bbf965e6072d9bc9478ed0
Prashanth <prashanth@IBOs-MacBook-Pro-3.local>
2023-06-10 10:00:19 +0530

    Testing

--- src/index.ts
+++ src/index.ts
@@ -15,3 +15,10 @@ values.push(4);
 
 let person:any[]=[1,"bro",true];
 person.push("India");
+
+// Tuples
+let table:[number,boolean,string]=[1,true,"KGF 2"];
+table.push(2);
+
+// Tuple array
+let employee:[number,string]=[2,"PSL"];
